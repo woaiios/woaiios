047d9b49b8dfb15558412e8b9e0cbe068e507448
ivan <abc@email.com>
2025-10-26 07:44:33 +0900

    Fix: Correct relative import path in AnalyzedText.js
    Corrected the import path for Component.js in web/WordDiscover/components/AnalyzedText/AnalyzedText.js from ./Component.js to ../Component.js. This resolves a 404 error when loading the module on GitHub Pages due to an incorrect relative path.

--- web/WordDiscover/components/AnalyzedText/AnalyzedText.js
+++ web/WordDiscover/components/AnalyzedText/AnalyzedText.js
@@ -1,4 +1,4 @@
-import { Component } from './Component.js';
+import { Component } from '../Component.js';
 
 export class AnalyzedTextComponent extends Component {
     constructor(selector, vocabularyManager) {
